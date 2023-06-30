d67876d7b8cf97fa32e30d873affdf403ff5f9fd
ibscontent <itbisys.content@gmail.com>
2023-06-29 23:04:20 -0400

    Adds bottom-up

---
+++ 1-using-bottom-up-stubs/PublishSoapService1/src/main/java/com/example/ws/jaxws/GetNameRequest.java
@@ -0,0 +1,36 @@
+
+package com.example.ws.jaxws;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "GetNameRequest", namespace = "http://ws.example.com/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "GetNameRequest", namespace = "http://ws.example.com/")
+public class GetNameRequest {
+
+    @XmlElement(name = "arg0", namespace = "")
+    private String arg0;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getArg0() {
+        return this.arg0;
+    }
+
+    /**
+     * 
+     * @param arg0
+     *     the value for the arg0 property
+     */
+    public void setArg0(String arg0) {
+        this.arg0 = arg0;
+    }
+
+}
